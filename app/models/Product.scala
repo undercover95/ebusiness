f61ed16d03ef6167423d0621ac83c1e4b491ddde
Michal <mbularz95@interia.pl>
2018-06-02 12:30:37 +0200

    refresh

--- app/models/Product.scala
+++
@@ -1,9 +0,0 @@
-package models
-
-import play.api.libs.json._
-
-case class Product(id: Long, name: String, description: String, category: Int, price: Float)
-
-object Product {
-  implicit val productFormat = Json.format[Product]
-}
